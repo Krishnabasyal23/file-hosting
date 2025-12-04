9b6fb912cde8cdc54da558b98d9409c36aa234d8
Krishna <krishnapdbashyal23@gmail.com>
2025-12-03 19:45:09 -0500

    add user model and connect MongoDB to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectDB= requrie("./config/db");
+connectDB();
 
 require("dotenv").config();
 //middlewares
