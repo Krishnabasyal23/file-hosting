29d0aa8b6af02c6181be6583f2c8d9fd2ac4fd87
Krishna <krishnapdbashyal23@gmail.com>
2025-12-07 03:30:23 -0500

    add protected file deletion route

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const router=express.Router();
 const upload= require ("../config/multer");
+const { deleteFile}= require("../controllers/fileController")
 const{
     uploadFile,
     getPublicFiles,
@@ -12,4 +13,5 @@ router.post("/upload", upload.single("file"), uploadFile);
 router.get("/public-files", getPublicFiles);
 router.get("/my-files", getMyFiles);
 router.delete("/files/:id", deleteFile);
+router.delete("/files/.id", authMiddleware, deleteFile);
 module.exports=router;
